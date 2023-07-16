cec60326cd44c835077b11f5c7b0b3d1988923fc
Andrea <andrea.sualo@gmail.com>
2023-07-16 18:47:35 +0200

    feat: create migration, model and factory of User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,11 +14,16 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('city');
+            $table->string('country');
             $table->rememberToken();
+            $table->unsignedBigInteger('role_id');
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
